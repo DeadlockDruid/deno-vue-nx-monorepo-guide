83d3c6587c30dcad06249cab21a45457d8ca0c78
Divyanshu Rathore <divyanshu03@live.com>
2025-01-17 14:15:34 +0530

    Fix lint errors

--- apps/backend/deno-app-2/main.ts
+++ apps/backend/deno-app-2/main.ts
@@ -7,8 +7,8 @@ app.get("/", (c) => {
   const DENO_APP_OWNER = Deno.env.get("DENO_APP_OWNER") || "Default Owner 2";
 
   return c.json({
-    message: `Deno App: ${DENO_APP} - Deno Owner Name: ${DENO_APP_OWNER} - dummy text 2`,
-`,
+    message:
+      `Deno App: ${DENO_APP} - Deno Owner Name: ${DENO_APP_OWNER} - dummy text 2`,
   });
 });
 
